c6feadb9568491aa266f8e2646ceaa24d292298b
Zhijie Zhang <zhijiezhang0124@gmail.com>
2018-11-21 17:35:54 -0800

    improve signature modal. Users can choose to store and use their signatures

--- src/components/SignatureModal/SignatureModal.js
+++ src/components/SignatureModal/SignatureModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import Button from 'components/Button';
 import ActionButton from 'components/ActionButton';
 
 import core from 'core';
@@ -24,18 +25,37 @@ class SignatureModal extends React.PureComponent {
     super();
     this.canvas = React.createRef();
     this.signatureTool = core.getTool('AnnotationCreateSignature');
+    this.state = { 
+      defaultSignatures: [],
+      isStoringSignature: false,
+      isAddingSignature: true
+    };
   }
 
   componentDidMount() {
     this.signatureTool.on('locationSelected', this.onLocationSelected);
+    this.signatureTool.on('annotationAdded', this.onSignatureAdded);
     this.signatureTool.setSignatureCanvas($(this.canvas.current));
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
     if (!prevProps.isOpen && this.props.isOpen) {
-      this.updateCanvasSize();
+      if (this.canvas.current) {
+        this.updateCanvasSize();
+      }
       this.props.closeElements([ 'printModal', 'loadingModal', 'errorModal' ]);
     }
+    
+    if (!prevState.isAddingSignature && this.state.isAddingSignature) {
+      this.signatureTool.setSignatureCanvas($(this.canvas.current));
+      this.updateCanvasSize();
+      this.signatureTool.openSignature();
+    }
+  }
+
+  componentWillUnmount() {
+    this.signatureTool.off('locationSelected', this.onLocationSelected);
+    this.signatureTool.off('annotationAdded', this.onSignatureAdded);
   }
 
   updateCanvasSize = () => {
@@ -47,30 +67,135 @@ class SignatureModal extends React.PureComponent {
     this.canvas.current.height = height * window.utils.getCanvasMultiplier();
   }
 
-  componentWillUnmount() {
-    this.signatureTool.off('locationSelected', this.onLocationSelected);
-  }
-
   onLocationSelected = () => {
     this.props.openElement('signatureModal');
-    this.signatureTool.openSignature();
+    if (this.canvas.current) {
+      this.signatureTool.openSignature();
+    }
+  }
+  
+  onSignatureAdded = (e, signatureAnnotation) => {
+    const defaultSignatures = [ ...this.state.defaultSignatures ];
+
+    if (this.state.isStoringSignature) {
+      const defaultSignature = {
+        Id: signatureAnnotation.Id,
+        img: this.getSignatureImage(signatureAnnotation)
+      };
+      
+      defaultSignatures.push(defaultSignature);
+      this.setState({
+        isStoringSignature: false,
+        defaultSignatures
+      });
+    }
+    this.setState({
+      isAddingSignature: !defaultSignatures.length
+    });
+
+    setTimeout(() => {
+      this.signatureTool.clearSignatureCanvas();
+    }, 0);
+  }
+
+  getSignatureImage = signatureAnnotation => {
+    return (
+      <img 
+        className="signature-image"
+        src={this.canvas.current.toDataURL()} 
+        onClick={() => this.addDefaultSignature(signatureAnnotation)}
+      >
+      </img>
+    );
   }
 
   closeModal = () => {
     this.signatureTool.clearSignatureCanvas();
     this.props.closeElement('signatureModal');
   }
-
+  
   clearCanvas = () => {
     this.signatureTool.clearSignatureCanvas();
     this.signatureTool.drawBackground();
   }
-
+  
   addSignature = () => {
     this.signatureTool.addSignature();
-    this.closeModal();
+    this.props.closeElement('signatureModal');
   }
 
+  addDefaultSignature = signatureAnnotation => {
+    this.signatureTool.freeHandAnnot = window.docViewer.getAnnotationManager().getAnnotationCopy(signatureAnnotation);
+    this.signatureTool.Gv = this.signatureTool.freeHandAnnot.getPaths().length;
+    this.signatureTool.addSignature();
+
+    this.props.closeElement('signatureModal');
+  }
+
+  handleCheckboxChange = () => {
+    this.setState(prevState => ({
+      isStoringSignature: !prevState.isStoringSignature
+    }));
+  }
+
+  switchModals = () => {
+    this.setState(prevState => ({
+      isAddingSignature: !prevState.isAddingSignature
+    }));
+  }
+
+  deleteDefaultSignature = Id => {
+    const defaultSignatures = this.state.defaultSignatures.filter(signature => signature.Id !== Id);
+
+    this.setState({
+      defaultSignatures,
+      isAddingSignature: !defaultSignatures.length 
+    });
+  }
+
+  renderSwitchModalButton = () => {
+    const { isAddingSignature, defaultSignatures } = this.state;
+    
+    if (isAddingSignature && defaultSignatures.length === 0) {
+      return null; 
+    }
+
+    const label = isAddingSignature ? 'Default Signatures' : 'Add Signature';
+    
+    return <Button label={label} onClick={this.switchModals} />;
+  }
+
+  renderAddSignatureModal = () => {
+    return(
+      <React.Fragment>
+        <canvas ref={this.canvas}></canvas>
+        <div className="footer">
+          <ActionButton dataElement="signatureModalClearButton" title="action.clear" img="ic_delete_black_24px" onClick={this.clearCanvas} />
+          <div className="footer__sign">
+            <input type="checkbox" checked={this.state.isStoringSignature} onChange={this.handleCheckboxChange} />
+            <p>Make Default Signature</p>
+            <ActionButton dataElement="signatureModalSignButton" title="action.sign" img="ic_check_black_24px" onClick={this.addSignature} />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  renderDefaultSignatureModal = () => {
+    return (
+      <React.Fragment>
+        <div className="default-signatures-modal">
+          {this.state.defaultSignatures.map(({ Id, img }) => (
+            <div key={Id} className="wrapper">
+              {img}
+              <ActionButton dataElement="signatureModalDeleteButton" title="action.delete" img="ic_delete_black_24px" onClick={() => this.deleteDefaultSignature(Id)} />
+            </div>
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
+  
   render() {
     if (this.props.isDisabled) {
       return null;
@@ -82,13 +207,13 @@ class SignatureModal extends React.PureComponent {
       <div className={className} onClick={this.closeModal}>
         <div className="container" onClick={e => e.stopPropagation()}>
           <div className="header">
+            {this.renderSwitchModalButton()}
             <ActionButton dataElement="signatureModalCloseButton" title="action.close" img="ic_close_black_24px" onClick={this.closeModal} />
           </div>
-          <canvas ref={this.canvas}></canvas>
-          <div className="footer">
-            <ActionButton dataElement="signatureModalClearButton" title="action.clear" img="ic_delete_black_24px" onClick={this.clearCanvas} />
-            <ActionButton dataElement="signatureModalSignButton" title="action.sign" img="ic_check_black_24px" onClick={this.addSignature} />
-          </div>
+          {this.state.isAddingSignature
+            ? this.renderAddSignatureModal()
+            : this.renderDefaultSignatureModal()
+          }
         </div>
       </div>
     );
